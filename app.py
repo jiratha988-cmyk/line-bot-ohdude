4f71e5db352efa41f85ceea7703f46ee254ef7f5
jiratha988-cmyk <jira.tha.988@gmail.com>
2025-08-06 12:59:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ def handle_message(event):
 
 # สำหรับรันใน local (Render จะไม่เรียกจากตรงนี้)
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
